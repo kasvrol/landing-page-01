ebb39c30530af7a97b0ccd3c0780fd95762207e7
kasvrol <dovalekarolina@gmail.com>
2022-01-06 20:13:28 -0300

    adição de componentes na primeira parte e adição de estilo

--- meu-app/src/components/Apresentacao/PrimeiraParte.jsx
+++ meu-app/src/components/Apresentacao/PrimeiraParte.jsx
@@ -1,22 +1,38 @@
 import React, {Component} from "react";
 import Frase from "../Textos/Frase";
 import ButtonStart from "../Button-Start/Button-Start";
+import "./apresentacao.css"
 
 class PrimeiraParte extends Component{
     render(){
         return(
-            <secton>
+            <secton className="body-primeira-parte">
                 <section>
-                    <h1>Lightning fast prototyping</h1>
+                    <h1 className="titulo-de-apresentacao">Lightning fast prototyping</h1>
                     <Frase/>
-                    <ButtonStart/>
-                    <button className="button-try">
-                        <a>Try For Free</a>
-                    </button>  
+                    <section className="buttons-options">
+                        <ButtonStart/>
+                        <button className="button-try">
+                            Try For Free
+                        </button> 
+                    </section> 
                 </section>
                 <section>
-                    <img src={"image/flat-browser.png"} alt="imagem do produto"/>
+                    <img src={"image/flat-browser.png"} alt="imagem do produto" className="imagem-do-produto"/>
                 </section>
+                <section>
+                    <h2 className="just-type">Just type</h2>
+                    <h1 className="titulo-de-apresentacao">Fastest way to navigate</h1>
+                    <Frase/>
+                </section>
+                <section className="buttons-options">
+                        <button>
+                            Download
+                        </button> 
+                        <button>
+                            See Walkthrough
+                        </button> 
+                    </section> 
             </secton>
         )
     }
