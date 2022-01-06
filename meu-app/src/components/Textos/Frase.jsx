6a4b33ff34a18824607b20a55871427916640cfd
kasvrol <dovalekarolina@gmail.com>
2022-01-06 15:26:17 -0300

    criação dos componentes de texto presente no design

---
+++ meu-app/src/components/Textos/Frase.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Frase extends Component{
+    render(){
+        return(
+            <article>
+            Most calendars are designed for teams. Slate is designed for freelancers.  
+            </article>
+        )
+    }
+
+}
+export default Frase;
---
+++ meu-app/src/components/Textos/Proposta.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Proposta extends Component{
+    render(){
+        return(
+            <article>
+            Most calendars are designed for teams. Slate is designed for freelancers who want a simple way to plan their schedule.  
+            </article>
+        )
+    }
+
+}
+export default Proposta;
