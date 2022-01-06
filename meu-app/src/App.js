fd832344e878186c3ed72511445a7a5bd0119f45
kasvrol <dovalekarolina@gmail.com>
2022-01-06 19:15:40 -0300

    adição da primeira parte da página ao arquivo app.js

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from "./components/Header/Header";
+import PrimeiraParte from "./components/Apresentacao/PrimeiraParte";
 import Pricing from "./components/Pricing/Pricing";
 import Footer from "./components/Footer/Footer";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <Header/>
+      <PrimeiraParte/>
       <Pricing/>
       <Footer/>
     </div>
--- meu-app/src/components/Apresentacao/PrimeiraParte.jsx
+++ meu-app/src/components/Apresentacao/PrimeiraParte.jsx
@@ -1,17 +1,22 @@
 import React, {Component} from "react";
-import Proposta from "../Textos/Frase";
+import Frase from "../Textos/Frase";
 import ButtonStart from "../Button-Start/Button-Start";
 
 class PrimeiraParte extends Component{
     render(){
         return(
             <secton>
-                <h1>Lightning fast prototyping</h1>
-                <Frase/>
-                <ButtonStart/>
-                <button className="button-try">
-                    <a>Try For Free</a>
-                </button>  
+                <section>
+                    <h1>Lightning fast prototyping</h1>
+                    <Frase/>
+                    <ButtonStart/>
+                    <button className="button-try">
+                        <a>Try For Free</a>
+                    </button>  
+                </section>
+                <section>
+                    <img src={"image/flat-browser.png"} alt="imagem do produto"/>
+                </section>
             </secton>
         )
     }
