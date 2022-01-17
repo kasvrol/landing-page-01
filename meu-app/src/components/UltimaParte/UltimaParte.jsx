056375d297d7381afae97876b9e952f8e81d1cfd
kasvrol <dovalekarolina@gmail.com>
2022-01-16 21:01:33 -0300

    adição do componente buttn start na ultima parte da página

--- meu-app/src/components/UltimaParte/UltimaParte.jsx
+++ meu-app/src/components/UltimaParte/UltimaParte.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import ConvitePart2 from "../Textos/ConvitePart2";
+import ButtonStart from "../Button-Start/Button-Start";
 import "./UltimaParte.css"
 
 class UltimaParte extends Component{
@@ -9,9 +10,12 @@ class UltimaParte extends Component{
                 <h1>
                 We focus on ergonomic meeting
                 </h1>
-                <section>
+                <section className="textos-da-ultma-parte">
                     <ConvitePart2/>
                     <ConvitePart2/>
+                    <div className="botao-ultima-parte">
+                        <ButtonStart/>
+                    </div>
                 </section>
             </div>
         )
